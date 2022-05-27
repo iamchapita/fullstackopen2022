132f3dab9e8c605e8d8084bdc179f04c8a1dc6c3
iamchapita <alejandrom646@gmail.com>
2022-05-26 20:00:07 -0600

    Implementación de llamado de funciones dentro de otras funciones

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Hello = () => {
+	return (
+		<div>
+			<p>Hola Mundo!</p>
+		</div>
+	);
+};
+
+const App = () => {
+	return (
+		<div>
+			<h1>Bienvenidos</h1>
+            <Hello/>
+		</div>
+	);
+};
+
+// La linea 16 se encarga de llamar a la variable Hello de 
+// la linea 4
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
