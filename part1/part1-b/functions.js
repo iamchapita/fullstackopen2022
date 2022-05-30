f436a676bcf51deb5b8989fd949cec1fb85a767f
iamchapita <alejandrom646@gmail.com>
2022-05-30 16:05:15 -0600

    Definición de funciones

---
+++ part1/part1-b/functions.js
@@ -0,0 +1,15 @@
+// -*- coding: utf-8 -*-
+/*
+@author: lamorales@unah.hn||iamchapita
+@date: 2022/05/30
+@version: 0.1.0
+*/
+
+const sum = (p1, p2) => {
+	console.log(p1);
+	console.log(p2);
+
+	return p1 + p2;
+};
+
+console.log(sum(2, 2));
