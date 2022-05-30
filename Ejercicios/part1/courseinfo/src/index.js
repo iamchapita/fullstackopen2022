fbe7ff2965315e5a196f54d7a88e3c85b2c80aeb
iamchapita <alejandrom646@gmail.com>
2022-05-30 16:23:38 -0600

    Cambio de estructura de definición de variables a objeto

--- Ejercicios/part1/courseinfo/src/index.js
+++ Ejercicios/part1/courseinfo/src/index.js
@@ -6,8 +6,8 @@ const Header = (props) => {
 		<div>
 			<h1>{props.course}</h1>
 		</div>
-	)
-}
+	);
+};
 
 const Content = (props) => {
 	return (
@@ -16,39 +16,64 @@ const Content = (props) => {
 				{props.part}: {props.exercises}
 			</p>
 		</div>
-	)
-}
+	);
+};
 
 const Total = (props) => {
 	return (
 		<div>
 			<p>Total de Ejercicios: {props.suma}</p>
 		</div>
-	)
-}
+	);
+};
 
 const App = () => {
-	const course = "Desarrollo de Aplicación usando Medio Stack";
-	const part1 = "Fundamentos de React";
-	const exercises1 = 10;
-	const part2 = "Usando Props para pasar data";
-	const exercises2 = 7;
-	const part3 = "Estado del Componente";
-	const exercises3 = 14;
+	const course = {
+		name: "Desarrollo de Aplicación usando MERN Stack",
+		parts: [
+			{
+				name: "Fundamentos de React",
+				exercises: 10,
+			},
+			{
+				name: "Usando Props para pasar data",
+				exercises: 7,
+			},
+			{
+				name: "Estado del Componente",
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course} />
-			<Content part={part1} exercises={exercises1} />
-			<Content part={part2} exercises={exercises2} />
-			<Content part={part3} exercises={exercises3} />
+			<Header course={course.name} />
+			<Content
+				part={course.parts[0]["name"]}
+				exercises={course.parts[0]["exercises"]}
+			/>
+			<Content
+				part={course.parts[1]["name"]}
+				exercises={course.parts[1]["exercises"]}
+			/>
+			<Content
+				part={course.parts[2]["name"]}
+				exercises={course.parts[2]["exercises"]}
+			/>
 
-			<Total suma={exercises1 + exercises2 + exercises3} />
+			<Total
+				suma={
+					course.parts[0]["exercises"] +
+					course.parts[1]["exercises"] +
+					course.parts[2]["exercises"]
+				}
+			/>
 		</div>
-	)
-}
+	);
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
