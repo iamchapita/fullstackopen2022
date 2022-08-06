dc95b609f108f4089c6c92a66cd07d46d215798b
iamchapita <alejandrom646@gmail.com>
2022-08-06 13:45:07 -0600

    Más sobre definición de funciones

--- part1/part1-b/functions.js
+++ part1/part1-b/functions.js
@@ -5,6 +5,7 @@
 @version: 0.1.0
 */
 
+// Definción de función flecha 
 const sum = (p1, p2) => {
 	console.log(p1);
 	console.log(p2);
@@ -13,3 +14,22 @@ const sum = (p1, p2) => {
 };
 
 console.log(sum(2, 2));
+
+
+// Usando map con un arreglo
+const t = [1,2,3,4];
+const tSquared = t.map(p=> p*p);
+
+console.log(tSquared);
+
+// Tipodes de definción de función 
+const average = (p1, p2) =>{
+    return (p1+p2)/2;
+};
+
+console.log(average(5,8));
+
+function average(p1, p2){
+    return (p1+p2)/2;
+}
+
