f931c208ad2183b1d457ad36133b13ec5ae843bf
iamchapita <alejandrom646@gmail.com>
2022-08-06 14:18:57 -0600

    Cambio de estructura para usar objetos definidos en arreglos

--- Ejercicios/part1/courseinfo/src/index.js
+++ Ejercicios/part1/courseinfo/src/index.js
@@ -13,7 +13,7 @@ const Content = (props) => {
 	return (
 		<div>
 			<p>
-				{props.part}: {props.exercises}
+				{props.name}
 			</p>
 		</div>
 	);
@@ -28,47 +28,31 @@ const Total = (props) => {
 };
 
 const App = () => {
-	const course = {
-		name: "Desarrollo de Aplicación usando MERN Stack",
-		parts: [
-			{
-				name: "Fundamentos de React",
-				exercises: 10,
-			},
-			{
-				name: "Usando Props para pasar data",
-				exercises: 7,
-			},
-			{
-				name: "Estado del Componente",
-				exercises: 14,
-			},
-		],
-	};
+	const course = 'Desarrollo de Aplicación usando MERN Stack';
+
+    const parts = [
+        {
+            name: 'Fundamentos de React',
+            exercises: 10
+        },
+        {
+            name: 'Usando props para pasar data',
+            exercises: 7
+        },
+        {
+            name: 'Estado de Componente',
+            exercises: 14
+        }
+    ];
 
 	return (
 		<div>
-			<Header course={course.name} />
-			<Content
-				part={course.parts[0]["name"]}
-				exercises={course.parts[0]["exercises"]}
-			/>
-			<Content
-				part={course.parts[1]["name"]}
-				exercises={course.parts[1]["exercises"]}
-			/>
-			<Content
-				part={course.parts[2]["name"]}
-				exercises={course.parts[2]["exercises"]}
-			/>
-
-			<Total
-				suma={
-					course.parts[0]["exercises"] +
-					course.parts[1]["exercises"] +
-					course.parts[2]["exercises"]
-				}
-			/>
+			<Header course={course} />
+			<Content name={parts[0].name} />
+			<Content name={parts[1].name} />
+			<Content name={parts[2].name} />
+            <Total suma={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> 
+            
 		</div>
 	);
 };
