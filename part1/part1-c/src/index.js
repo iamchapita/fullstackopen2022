44bec6d0d8ce952aca0d0adac6e3a137cb459088
iamchapita <alejandrom646@gmail.com>
2022-08-08 04:00:25 -0600

    Re-renderizado con temporizador y contador

--- part1/part1-c/src/index.js
+++ part1/part1-c/src/index.js
@@ -2,33 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Ya no se recibe el objeto props, si no dos variables con los datos
-const Hello = ({name, age}) => {
+const Hello = ({ name, age }) => {
+	// Esta es una función auxiliar del componente
+	const bornYear = () => {
+		const yearNow = new Date().getFullYear();
+		return yearNow - age;
+	};
 
-    // Esta es una función auxiliar del componente 
-    const bornYear = () => {
-        const yearNow = new Date().getFullYear();
-        return yearNow - age;
-    };
-
-    return(
-        <div>
-            <p>Hola {name} tienes {age} años</p>
-            <p>Naciste en {bornYear()}</p>  
-        </div>
-    );
-}
-
-const App = () => {
-
-    const name = 'Alejandro';
-    const age = 24;
-
-    return (
-        <div>
-            <h1>Hola Mundo</h1>
-            <Hello name={name} age={age}/>
-        </div>
-    );
+	return (
+		<div>
+			<p>
+				Hola {name} tienes {age} años
+			</p>
+			<p>Naciste en {bornYear()}</p>
+		</div>
+	);
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let counter = 1;
+
+const refresh = () => {
+	ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
+};
+
+// Una funcion flecha dentro de un metodo de alguna clase 
+setInterval(
+    () => {
+        refresh();
+        counter += 1;
+    }, 1000
+)
+
+// Incluimos funciones para renderizado de la pagina de forma dinamica
+const App = ({ counter }) => {
+	return <div>{counter}</div>;
+};
