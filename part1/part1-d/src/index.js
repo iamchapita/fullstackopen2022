0b0e3e0a1ede082b1a7547d4539a1775d0040a69
iamchapita <alejandrom646@gmail.com>
2022-08-31 12:15:36 -0600

    Incluyendo dos variables de estado

--- part1/part1-d/src/index.js
+++ part1/part1-d/src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
+const App = () => {
 
-const App = () =>{
-    return (
-        <div>
-            <p>
-                Prueba
-            </p>
-        </div> 
-    );
-}
+    // Dos partes de estado 
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
 
+	return (
+		<div>
+			{left}
+            <button onClick={() => setLeft(left + 1)}>Left</button> 
+            <button onClick={() => setRight(right + 1)}>Right</button>
+            {right}
+		</div>
+	);
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
