72ad8d3e5cddfbcfdda31e0f2b47f6cbc720b7c1
iamchapita <alejandrom646@gmail.com>
2023-05-11 14:53:25 -0600

    Implementacion de recursos necesarios

---
+++ part2/e/src/components/Note.js
@@ -0,0 +1,24 @@
+// -*- coding: utf-8 -*-
+/*
+@author: lamorales@unah.hn || alejandrom646@gmail.com ||iamchapita
+@date: 2022/12/26
+@version: 0.1.0
+*/
+
+import React from "react";
+
+const Note = ({ note, toggleImportance }) => {
+    
+    const label = note.important ? 'Make not Important' : 'Make important';
+
+    return (
+		<div>
+			<li> 
+                {note.content} 
+                <button onClick={toggleImportance}>{label}</button>
+            </li>
+		</div>
+	);
+};
+
+export default Note;
